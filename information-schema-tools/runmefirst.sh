c4f53c823be9e22e45785bd84b07944fb1d85260
Brian Suk <briansuk@google.com>
2020-11-02 11:04:40 -0500

    added second example table

--- information-schema-tools/runmefirst.sh
+++ information-schema-tools/runmefirst.sh
@@ -3,4 +3,12 @@ bq mk \
 --table \
 --expiration 3600 \
 --description "Helo, first table!" \
---label category:cool-data, ingestion-method:manual > hellometadata.first_table 
+--label category:cool-data --label ingestion-method:manual \
+hellometadata.first_table table1_schema
+bq mk -t \
+--time_partitioning_type=DAY \
+--time_partitioning_expiration 259200 \
+--description "This is my time partitioned table" \
+--label organization:development --label category:cool-data \
+hellometadata.second_table \
+table2_schema
