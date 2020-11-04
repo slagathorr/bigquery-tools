8218f4c8d3b5e478611c7e01386887dd571f0c1c
Brian Suk <briansuk@google.com>
2020-11-04 10:21:35 -0500

    completed first pass of CSV generator to local filesystem

--- fixed-width-parser/fw_process.py
+++ fixed-width-parser/fw_process.py
@@ -2,7 +2,9 @@ import struct
 import csv
 
 # Create Schema
-with open('./data/brfss_format.csv') as schema_file:
+column_list = []
+
+with open('./test_data/brfss_format.csv', mode = 'r') as schema_file:
   csv_reader = csv.reader(schema_file, delimiter=",")
   line_count = 0
   for row in csv_reader:
@@ -11,20 +13,29 @@ with open('./data/brfss_format.csv') as schema_file:
       print row
       line_count += 1
     else:
-      print "Row: "
-      print row
+      column_list.append({"name": row[0], "start": int(row[1]), "length": int(row[2])})
+      print (row[0], row[1], row[2])
+
+# Read through input data, and write rows to output file in CSV.
+with open('./test_data/LLCP2019_TOP10ROWS.ASC', mode = 'r') as input_file:
+  with open('./test_data/LLCP2019_TOP10ROWS.CSV', mode = 'w') as output_file:
+    line_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    
+    # Print the header row.
+    column_header = []
+    for iter in column_list:
+      column_header.append(iter["name"])
+    line_writer.writerow(column_header) # Write the line
+
+     # Start traversing the source file.
+    line_count = 0
+    for input_line in input_file:
+      out_line = []
+      for iter in column_list: # Run through the list of columns and get their values.
+        out_line.append(str.strip(input_line[iter["start"] - 1:iter["start"] - 1 + iter["length"]]))
+      line_writer.writerow(out_line) # Write the line.
       line_count += 1
-  print "Processed: "
-  print line_count
-
-fieldwidths = (2,2,8,2,2,4,4)  # negative widths represent ignored padding fields
-fmtstring = ' '.join('{}{}'.format(abs(fw), 'x' if fw < 0 else 's')
-                        for fw in fieldwidths)
-print fmtstring
-fieldstruct = struct.Struct(fmtstring)
-parse = fieldstruct.unpack_from
-print('fmtstring: {!r}, recsize: {} chars'.format(fmtstring, fieldstruct.size))
-
-line = "01              0101182019     11002019000001                 11 121 0120001              2         31588881121112112222 222223  1121207                                    232        12127880301540502 22212213 073888      2                2101025553022012033152      412      213                                                                                                 134 42           22221131111                                                                                                                                                                                                                                                                           1001                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      11     101101146.9849076                           0.523.4924538     02                                                                                                                                                                                                                 1 0.5617998190.819282                                                 135.30408                                                                                                                                          13192122113112                                                        0202  2222221328060621570698528173211231200010000012      0176600303                            023330                              4233213200000200000700140043005000110002000001141111002121"
-fields = parse(line)
-print('fields: {}'.format(fields))
+      if line_count % 1000 == 0:
+        print ("Processed: ", line_count)
+    print "Processed: "
+    print line_count
