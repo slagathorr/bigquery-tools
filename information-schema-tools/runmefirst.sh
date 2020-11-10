888742c829d2ce0d1181984595204f32771a3c2b
Brian Suk <briansuk@google.com>
2020-11-10 11:35:25 -0500

    updated table expiration to be longer

--- information-schema-tools/runmefirst.sh
+++ information-schema-tools/runmefirst.sh
@@ -1,7 +1,7 @@
 bq mk --dataset --default_table_expiration 3600 --description "Hello, metadata world!" hellometadata
 bq mk \
 --table \
---expiration 3600 \
+--expiration 360000 \
 --description "Helo, first table!" \
 --label category:cool-data --label ingestion-method:manual \
 hellometadata.first_table table1_schema
