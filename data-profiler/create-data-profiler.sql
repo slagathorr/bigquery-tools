e28f43588bcc56247c7e56636274a86387973070
Brian Suk <briansuk@google.com>
2020-11-30 12:17:15 -0500

    fixed formatting

--- data-profiler/create-data-profiler.sql
+++ data-profiler/create-data-profiler.sql
@@ -22,9 +22,9 @@ BEGIN
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-    */
+  */
 
-    /*
+  /*
     READ ME FIRST:
     Be sure to understand BigQuery pricing, and the plan that you are on, as you
     will likely be incurring query costs to your project that you are responsible for.
