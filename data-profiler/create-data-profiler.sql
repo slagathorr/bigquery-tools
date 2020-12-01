79ac89e1226aae693ea951611d52332413834a52
Brian Suk <briansuk@google.com>
2020-12-01 09:32:52 -0500

    refactored profiler, extracted min, max, mean, median, nulls from loop

--- data-profiler/create-data-profiler.sql
+++ data-profiler/create-data-profiler.sql
@@ -1,7 +1,7 @@
 #bigquery
 
 CREATE OR REPLACE PROCEDURE
-  data_profiler_test.ProfileTable(
+  data_profiles.ProfileTable( # REPLACE data_profiles WITH YOUR DATASET
     full_source_table_name STRING,
     full_destination_table_name STRING,
     max_groups INT64
@@ -22,9 +22,9 @@ BEGIN
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-  */
+    */
 
-  /*
+    /*
     READ ME FIRST:
     Be sure to understand BigQuery pricing, and the plan that you are on, as you
     will likely be incurring query costs to your project that you are responsible for.
@@ -39,9 +39,13 @@ BEGIN
         Ex: `hereismyorg.hereismydataset.hello_profile`
       max_groups:
         Maximum number of groups for certain profile tasks, such as pattern analysis.
-  */
+    */
   
   # DECLARE Working Variables
+  DECLARE profile_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP();
+  DECLARE profile_id STRING DEFAULT GENERATE_UUID();
+  DECLARE query_string STRING;
+  
   DECLARE total_row_count INT64;
 
   DECLARE null_count INT64;
@@ -88,8 +92,10 @@ BEGIN
 
   # Create Profile Results Table
   EXECUTE IMMEDIATE
-    FORMAT("""CREATE OR REPLACE TABLE %s 
+    FORMAT("""CREATE TABLE IF NOT EXISTS %s 
     (
+      profile_id STRING,
+      profile_timestamp TIMESTAMP,
       column_name STRING,
       column_distribution ARRAY<STRUCT<value STRING, raw_count INT64, percentage NUMERIC>>,
       null_analysis STRUCT<null_count INT64, null_percentage NUMERIC, non_null_count INT64, non_null_percentage NUMERIC>,
@@ -103,7 +109,86 @@ BEGIN
     FORMAT("SELECT COUNT(*) FROM %s", full_source_table_name)
     INTO total_row_count;
 
+  # Build query for min/max, nulls, mean/median
+  SET query_string = "CREATE TEMP TABLE numeric_calculation_results AS (";
+  WHILE
+    column_iterator < ARRAY_LENGTH(columns_to_profile)
+  DO
+    SET
+      query_string = query_string ||
+        IF
+          (column_iterator != 0," UNION ALL ","");
+    SET
+      query_string = query_string 
+        || "SELECT \"" 
+          # column name
+          || columns_to_profile[OFFSET(column_iterator)] || "\" AS column_name," 
+
+          # min/max
+          || "SAFE_CAST(MIN(" || columns_to_profile[OFFSET(column_iterator)] || ") AS STRING) AS min," 
+          || "SAFE_CAST(MAX(" || columns_to_profile[OFFSET(column_iterator)] || ") AS STRING) AS max,"
+
+          # nulls
+          || "SUM(IF(" || columns_to_profile[OFFSET(column_iterator)] || " IS NULL,1,0)) AS null_count," 
+          || "SUM(IF(" || columns_to_profile[OFFSET(column_iterator)] || " IS NULL,0,1)) AS non_null_count,"
+
+          # mean/median
+          || "IFNULL(ROUND(AVG(SAFE_CAST(SAFE_CAST(" || columns_to_profile[OFFSET(column_iterator)] || " AS STRING) AS FLOAT64)), 6), -1.0) AS mean," 
+          || "(SELECT IFNULL(ROUND((PERCENTILE_CONT(SAFE_CAST(SAFE_CAST("
+            || columns_to_profile[OFFSET(column_iterator)] || " AS STRING) AS FLOAT64), 0.5) OVER()), 6), -1.0) "
+            || "FROM " || full_source_table_name || " LIMIT 1) AS median";
+    SET
+      query_string = query_string || " FROM " || full_source_table_name;
+    SET
+      column_iterator = column_iterator + 1;
+  END WHILE;
+  SET query_string = query_string || ");";
+
+  EXECUTE IMMEDIATE query_string;
+  
+  # Write results to table
+  EXECUTE IMMEDIATE
+    FORMAT("""
+      INSERT %s
+        (profile_id, profile_timestamp, column_name, null_analysis, numeric_analysis, min_max)
+      SELECT
+        "%s" as profile_id,
+        @var_profile_timestamp as profile_timestamp,
+        column_name,
+        
+        #null_analysis
+        STRUCT(null_count, 
+          CAST(ROUND(safe_multiply(
+          safe_divide(
+            null_count,
+            %d),
+          100), 6) AS NUMERIC) AS null_percentage,
+          non_null_count,
+          CAST(ROUND(safe_multiply(
+          safe_divide(
+            non_null_count,
+            %d),
+          100), 6) AS NUMERIC) AS non_null_percentage)
+          AS null_analysis,
+        
+        # numeric_analysis
+        STRUCT(SAFE_CAST(IF(mean = -1.0, NULL, mean) AS NUMERIC) AS mean, 
+          SAFE_CAST(IF(median = -1.0, NULL, median) AS NUMERIC) AS median)
+          AS numeric_analysis,
+        
+        # min_max
+        STRUCT(min, max) AS min_max
+      FROM
+        numeric_calculation_results;
+    """,
+    full_destination_table_name,
+    profile_id,
+    total_row_count, total_row_count)
+    USING
+    profile_timestamp as var_profile_timestamp;
+
   # START Processing
+  SET column_iterator = 0;
   WHILE
     column_iterator < ARRAY_LENGTH(columns_to_profile) DO
 
@@ -133,35 +218,6 @@ BEGIN
       columns_to_profile[OFFSET(column_iterator)],
       max_groups)
       INTO var_pattern_distribution;
-
-    # Calculate median
-    EXECUTE IMMEDIATE
-      FORMAT("""
-      SELECT
-        IFNULL(ROUND((PERCENTILE_CONT(SAFE_CAST(SAFE_CAST(%s AS STRING) AS FLOAT64), 0.5) OVER()), 6), -1.0) AS median
-      FROM %s
-      LIMIT 1""",
-      columns_to_profile[OFFSET(column_iterator)],
-      full_source_table_name) INTO median;
-
-    # Calculate min, max, mean, null, non-null.
-    EXECUTE IMMEDIATE
-      FORMAT("""
-      SELECT
-        SAFE_CAST(MIN(%s) AS STRING), 
-        SAFE_CAST(MAX(%s) AS STRING),
-        IFNULL(ROUND(AVG(SAFE_CAST(SAFE_CAST(%s AS STRING) AS FLOAT64)), 6), -1.0),
-        SUM(IF(%s IS NULL,1,0)),
-        SUM(IF(%s IS NULL,0,1))
-      FROM %s""",
-      columns_to_profile[OFFSET(column_iterator)],
-      columns_to_profile[OFFSET(column_iterator)],
-      columns_to_profile[OFFSET(column_iterator)],
-      columns_to_profile[OFFSET(column_iterator)],
-      columns_to_profile[OFFSET(column_iterator)],
-      full_source_table_name)
-      INTO
-      min, max, mean, null_count, non_null_count;
 
     # Calculate inferred data type
     EXECUTE IMMEDIATE
@@ -217,48 +273,29 @@ BEGIN
       )
       INTO var_inferred_data_type;
 
+    EXECUTE IMMEDIATE "SELECT @var_pattern_distribution" USING var_pattern_distribution as var_pattern_distribution;
+
     # Write results.
     EXECUTE IMMEDIATE FORMAT("""
-      INSERT %s (column_name, column_distribution, null_analysis, numeric_analysis, min_max, inferred_data_type)
-      VALUES(
-        # column_name
-        "%s", 
-        
+      UPDATE %s
+      SET 
         # column_distribution
-        (SELECT @var_pattern_distribution),
-        
-        #null_analysis
-        STRUCT(%d, 
-          CAST(ROUND(safe_multiply(
-          safe_divide(
-            %d,
-            %d),
-          100), 6) AS NUMERIC) AS null_percentage,
-          %d,
-          CAST(ROUND(safe_multiply(
-          safe_divide(
-            %d,
-            %d),
-          100), 6) AS NUMERIC) AS non_null_percentage),
-        
-        # numeric_analysis
-        STRUCT(SAFE_CAST(IF(%f = -1.0, NULL, %f) AS NUMERIC) AS mean, 
-          SAFE_CAST(IF(%f = -1.0, NULL, %f) AS NUMERIC) AS median),
-        
-        # min_max
-        STRUCT("%s" AS min, "%s" AS max),
+        column_distribution = (SELECT @var_pattern_distribution),
         
         # type
-        (SELECT @var_inferred_data_type)
-      );""",
+        inferred_data_type = (SELECT @var_inferred_data_type)
+      WHERE
+        column_name = "%s"
+        AND profile_id = "%s"
+        AND profile_timestamp = @var_profile_timestamp
+        
+      ;""",
       full_destination_table_name, 
       columns_to_profile[OFFSET(column_iterator)],
-      null_count, null_count, total_row_count,
-      non_null_count, non_null_count, total_row_count,
-      mean, mean, median, median,
-      min, max
+      profile_id
       )
       USING 
+      profile_timestamp as var_profile_timestamp,
       var_pattern_distribution as var_pattern_distribution,
       var_inferred_data_type as var_inferred_data_type;
 
