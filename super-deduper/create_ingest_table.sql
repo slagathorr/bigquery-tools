b9b0f1f7d21469e43488a761c143a9119fc6e1ef
Brian Suk <briansuk@google.com>
2020-12-29 12:42:01 -0500

    added sproc for superdeduper

--- super-deduper/create_ingest_table.sql
+++ super-deduper/create_ingest_table.sql
@@ -5,4 +5,4 @@ SELECT DATE(2020,2,15) ingest_ts, DATE(2020,1,15) reported_dt, 'California' COLA
 UNION ALL SELECT DATE(2020,2,15), DATE(2020,1,15), 'California', '22-30', 100, 'pong' # insert
 UNION ALL SELECT DATE(2020,2,15), DATE(2020,1,15), 'Delaware', '22-30', 200, 'ping' # duplicate ignore
 UNION ALL SELECT DATE(2020,2,15), DATE(2020,1,15), 'Florida', '18-22', 500, 'And I think to myself What a wonderful world' # insert
-UNION ALL SELECT DATE(2020,2,15), DATE(2020,1,15), 'California', '18-22', 100, 'hello' # duplicate ignore;
+UNION ALL SELECT DATE(2020,2,15), DATE(2020,1,15), 'California', '18-22', 100, 'hello'; # duplicate ignore
--- super-deduper/create_main_table.sql
+++ super-deduper/create_main_table.sql
@@ -1,4 +1,5 @@
 # Base Data
-SELECT TRUE is_latest, DATE(2020,1,15) ingest_ts, DATE(2020,1,15) reported_dt, "California" COLA, "18-22" COLB, 100 MEASUREMENT, "hello" COLC
+SELECT FALSE is_latest, DATE(2020,1,15) ingest_ts, DATE(2020,1,15) reported_dt, "California" COLA, "18-22" COLB, 100 MEASUREMENT, "hello" COLC
+UNION ALL SELECT TRUE, DATE(2020,1,16), DATE(2020,1,15), "California", "18-22", 150, "hello"
 UNION ALL SELECT TRUE, DATE(2020,1,15), DATE(2020,1,15), "California", "31-50", 500, "world"
 UNION ALL SELECT TRUE, DATE(2020,1,15), DATE(2020,1,15), "Delaware", "22-30", 200, "ping";
